e65384da8a018d0794d44872a5f597c4e58023f1
davidinmichael <davidinmichael@gmail.com>
2024-12-11 10:27:30 +0100

    first commit

---
+++ bookcraftafrica/settings/local.py
@@ -0,0 +1,16 @@
+from .base import *
+import os
+from dotenv import load_dotenv
+
+load_dotenv
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
