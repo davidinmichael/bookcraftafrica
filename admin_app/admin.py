fd58717a0ed8556220002836296aad24623e27c5
davidinmichael <davidinmichael@gmail.com>
2024-12-13 09:41:35 +0100

    Added the Book, Blog and Event Models.

--- admin_app/admin.py
+++ admin_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import (
+	HeaderSlider, Book, Blog, Event
+)
 
-# Register your models here.
+
+admin.site.register([HeaderSlider, Book, Blog, Event])
--- admin_app/models.py
+++ admin_app/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.utils.text import slugify
 
+
+class BookCategory(models.TextChoices):
+	FICTION = "fiction", _("Fiction")
+	NON_FICTION = "non_fiction", _("Non Fiction")
+	ADVENTURE = "adventure", _("Adventure")
+	KIDS = "kids", _("Kids")
+	TECHNOLOGY = "technology", _("Technology")
+	OTHERS = "others", _("Others")
 
 
 class BaseModel(models.Model):
@@ -20,3 +30,60 @@ class HeaderSlider(BaseModel):
 
 	def __str__(self):
 		return f"{self.title} | {self.subtitle}"
+
+
+class Book(BaseModel):
+	title = models.CharField(max_length=100)
+	author = models.CharField(max_length=50, null=True, blank=True)
+	description = models.TextField()
+	category = models.CharField(max_length=50, null=True, blank=True, choices=BookCategory.choices, default=BookCategory.OTHERS)
+	pages = models.CharField(max_length=10, null=True, blank=True)
+	year_published = models.CharField(max_length=5, null=True, blank=True)
+	book_type = models.CharField(max_length=20, null=True, blank=True)
+	book_cover = models.ImageField(upload_to="book_cover/")
+	snippet = models.TextField(null=True, blank=True)
+	slug = models.SlugField()
+
+	def save(self, *args, **kwargs):
+		if not self.slug:
+			base_slug = slugify(self.title)
+			self.slug = f"{base_slug}-{self.id}"
+
+		if not self.snippet:
+			self.snippet = ' '.join(self.description.split()[:20])
+		return super().save(*args, **kwargs)
+
+	def __str__(self):
+		return f"{self.title} | {self.author}"
+
+
+class Blog(BaseModel):
+	title = models.CharField(max_length=100)
+	blog_body = models.TextField()
+	blog_img = models.ImageField(upload_to="blog_img/", null=True, blank=True)
+	snippet = models.TextField(null=True, blank=True)
+	slug = models.SlugField()
+
+	def save(self, *args, **kwargs):
+		if not self.slug:
+			base_slug = slugify(self.title)
+			self.slug = f"{base_slug}-{self.id}"
+
+		if not self.snippet:
+			self.snippet = ' '.join(self.description.split()[:20])
+		return super().save(*args, **kwargs)
+
+	def __str__(self):
+		return f"{self.title}"
+
+
+class Event(BaseModel):
+	title = models.CharField(max_length=100)
+	description = models.TextField()
+	event_date = models.DateField(null=True, blank=True)
+	event_time = models.TimeField(null=True, blank=True)
+	event_img = models.ImageField(upload_to="event_img/", null=True, blank=True)
+
+	def __str__(self):
+		return f"{self.title}"
+
