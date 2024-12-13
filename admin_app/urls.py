0c77ffc62d68ed96a5c1ba037bb1c30a0f2ed445
davidinmichael <davidinmichael@gmail.com>
2024-12-13 09:56:11 +0100

    Added urls for adding book, header, blog and event.

--- admin_app/urls.py
+++ admin_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import (
-	AddPlatformItems
+	AddPlatformItems, HeaderSliderView, BookView,
+	BlogView, EventView
 )
 
 urlpatterns = [
-	path("add-items/", AddPlatformItems.as_view(), name="add-items"),
+	path("add-items/", AddPlatformItems.as_view(), name="add_items"),
+	path("add-header/", HeaderSliderView.as_view(), name="add_header"),
+	path("add-book/", BookView.as_view(), name="add_book"),
+	path("add-blog/", BlogView.as_view(), name="add_blog"),
+	path("add-event/", EventView.as_view(), name="add_event"),
 ]
